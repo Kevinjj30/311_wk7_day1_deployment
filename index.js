4e1c6db0fb55d6b149c82e2506d9d2e764ef00ae
Kevinjj30 <48122752+Kevinjj30@users.noreply.github.com>
2019-12-16 19:24:44 -0600

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use('/users', usersRouter)
 app.use('/auth', authRouter)
 
 app.get('/', (req, res) => {
-  res.send('Welcome to my testing server!')
+  res.send('Welcome to my testing again server!')
 })
 
 app.listen(port, () => {
